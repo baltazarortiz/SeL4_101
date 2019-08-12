8e31a726603fc9ae88229e0262860b747a047dbf
Baltazar Ortiz <baltazar@mit.edu>
2019-08-12 17:34:47 -0400

    clone riscv-pk in case riscv is being targeted

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone --single-branch --branch 10.1.1 https://github.com/seL4/seL4.git kernel
 git clone --single-branch --branch 10.1.x-compatible https://github.com/seL4/seL4_tools.git tools
+git clone --single-branch --branch 10.1.x-compatible https://github.com/SEL4PROJ/riscv-pk.git tools/riscv-pk 
 
 mkdir projects
 git clone --single-branch --branch 10.1.x-compatible https://github.com/seL4/seL4_libs.git projects/seL4_libs 
